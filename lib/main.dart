145ea608571c2e264409132b62e93d98d13c4f95
Sergio vega <80370310+siuloirges@users.noreply.github.com>
2022-01-27 09:08:03 -0500

    feat: create main
    Se implemento la logica para el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:oktoast/oktoast.dart';
 import 'package:provider/provider.dart';
+import 'package:proyect_test/src/modules/posts/provider/posts_provider.dart';
 import 'package:proyect_test/src/utils/components/ui/styles/styles.dart';
-import 'package:proyect_test/src/utils/providers/authentication_provider.dart';
+import 'package:proyect_test/src/modules/load_picture/provider/load_picture_provider.dart';
 import 'package:proyect_test/src/utils/routes/routes.dart';
 
-
-
-void main() async {
-
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
-}
+void main() async => runApp(const MyApp()); 
 
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
@@ -21,13 +16,16 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
-          create: (context) => AuthenticationProvider(),
+          create: (context) => LoadPictureProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) =>  PostsProvider(),
         ),
       ],
       child: OKToast(
         handleTouch: true,
         child: MaterialApp(
-         
+          locale: const Locale('es', 'CO'),
           debugShowCheckedModeBanner: false,
           title: 'Technical Test',
           initialRoute: 'login',
