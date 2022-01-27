0c3a69cb9646c7cc99b4dc24098c1692c9d2fade
Sergio vega <80370310+siuloirges@users.noreply.github.com>
2022-01-27 09:13:31 -0500

    feat: module sigUp
    se implemento este archivo con el fin de crear un registro de usuario para posteriormenten iniciar en el login

--- lib/src/modules/sign_up/ui/screen/register_page.dart
+++ lib/src/modules/sign_up/ui/screen/register_page.dart
@@ -2,31 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:proyect_test/src/modules/sign_up/ui/widgets/body_login_page.dart';
 import 'package:proyect_test/src/utils/components/ui/styles/styles.dart';
-import 'package:proyect_test/src/utils/providers/authentication_provider.dart';
+import 'package:proyect_test/src/modules/load_picture/provider/load_picture_provider.dart';
 
 class RegisterPage extends StatelessWidget {
   RegisterPage({Key key}) : super(key: key);
   static String pageName = "home_page";
   Size size; 
-  AuthenticationProvider authenticationProvider;
   @override
   Widget build(BuildContext context) {
-    authenticationProvider = Provider.of<AuthenticationProvider>(context);
     size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: const [
-              Icon(Icons.login_rounded,color: Colors.white,),
-              Text(' Login'),
+              Icon(Icons.app_registration_rounded,color: Colors.white,),
+              Text(' LognUp'),
           ],
         ),
         elevation: 0,
         centerTitle: true,
         backgroundColor: primaryColor,
       ),
-      body: SingleChildScrollView(child: BodyRegisterPage(key: const Key('Rg1'),)),
+      body: const SingleChildScrollView(child: BodyRegisterPage(key: Key('Rg1'),)),
     );
   }
 }
